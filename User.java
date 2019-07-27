a44d2ac812724b8aed2effed47e61a6a833b61ca
YifeiGao <52712330+YifeiGao@users.noreply.github.com>
2019-07-27 19:41:55 -0400

    Update User.java

--- User.java
+++ User.java
@@ -10,81 +10,98 @@ public class User {
 	private static final String[] user_column_type = {"VARCHAR(30) NOT NULL", "VARCHAR(30) NOT NULL", "VARCHAR(30) NOT NULL", "INT NOT NULL", "VARCHAR(30) NOT NULL", "INT NOT NULL", "INT DEFAULT 0", "VARCHAR(30) NOT NULL","INT DEFAULT 0"};
 	private static final String user_primary_key = "user_name";
 
-	private static String user_id;
+	private String curr_user;
 
-	public static SQLController sqlMngr = null;
+	public static SQLController sqlMngr = new SQLController();
+	public User(){
+		try {
+			sqlMngr.connect();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Esception occurs in User.constructor");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	/**
 	 * function will be operate after connected successfully, it handle user's login and register
 	 * String choice: the use's choice which have 2 possible values: 1 for login and 2 for register
 	 * boolean check: a boolean type that check if the user typed the number that are provided
 	 */
-	public void userStart(SQLController sqlC){
-		sqlMngr = sqlC;
+	public boolean userStart(){
 		String choice;
-		boolean check = false;
+		boolean check;
+		boolean log_in;
 		choice = CommandLine.sc.nextLine();
 		System.out.println("Welcome to Mybnb, if you want to login press 1, if you are not a user yet please press 2 to register");
-		while (!check){
-			check = choice.equals("1") | choice.equals("2");
-		}
+		do{
+			check = choice.equals("1") || choice.equals("2");
+		}while(!check);
 		if (choice.equals("1")){
 			//login operations
-			this.login();
+			log_in = this.login();
 		}
-		else if (choice.equals("2")){
+		else{
 			//create a user
-			this.createUser();
+			log_in = this.createUser();
 		}
+		return log_in;
 	}
 	/**
 	 * the function that handle login
 	 * String pass: the password that the user typed in the command line
 	 * String sql: the Mysql query that need to be executed
 	 */
-	private void login(){
+	private boolean login(){
 		String pass;
 		String sql;
-		ArrayList <ArrayList<String>> result = new ArrayList <ArrayList<String>>();
+		String result;
 		boolean check_pass;
-		int choice = 0;
+		int choice;
 		String user_name;
+		boolean log_in = false;
 		//check if the userID that the user typed exist, if not, type again or go regist
 		do{
 			System.out.println("Please type your username below");
 			user_name = CommandLine.sc.nextLine();
-			if(!CommandLine.sqlMngr.checkExist("*", new String[] {"user_name"}, new String[] {user_name}, "users")){
+			choice = 0;
+			if(!sqlMngr.checkExist("*", new String[] {"user_name"}, new String[] {user_name}, "users")){
 				System.out.println("Sorry the user id is not found, please type 1 to try again or type 2 to regist");
 				do{
 					System.out.print("Pleas type your choice [1-2]");
 					choice = Integer.parseInt(CommandLine.sc.nextLine());
 				}while(choice != 2 || choice != 1);
-				switch(choice){
-				case 1:
-					//the user want to type the user name again
-					break;
-				case 2:
-					//go to create a user and exit the login function
-					this.createUser();
-					System.out.println("Going to regist");
-					return;
-				}
 			}
-		}while(choice == 1);
-		//check if the password that the user typed is matched according to the database
-		do{
-			System.out.println("Please type your password below");
-			pass = CommandLine.sc.nextLine();
-			sql = "SELECT password FROM users WHERE user name = '"+user_name+"';";
-			result = CommandLine.sqlMngr.rsToList(CommandLine.sqlMngr.selectOp(sql));	
-			check_pass = (result.get(0).equals(pass));
-			if(!check_pass){
-				System.out.println("incorrect password");
+		}while(choice == 1 && !sqlMngr.checkExist("*", new String[] {"user_name"}, new String[] {user_name}, "users"));
+		if(choice == 2){
+			log_in = this.createUser();
+			return log_in;
+		}
+		else{
+			try{
+				//check if the password that the user typed is matched according to the database
+				do{
+					System.out.println("Please type your password below");
+					pass = CommandLine.sc.nextLine();
+					sql = "SELECT password FROM users WHERE user name = '"+user_name+"';";
+					result = sqlMngr.selectOp(sql).getString("password");
+					check_pass = (result.equals(pass));
+					if(!check_pass){
+						System.out.println("incorrect password");
+					}
+				}while (!check_pass);
+			}catch(SQLException e){
+				System.err.println("Exception in User.login");
+				e.printStackTrace();
 			}
-		}while (!check_pass);
-		//set the user name as current user if log in successfully
-		this.setUser(user_name);
-		System.out.println("Logged in , please choose a options provided below");
-		this.operations();
+			//set the user name as current user if log in successfully
+			this.setUser(user_name);
+			//System.out.println("Logged in , please choose a options provided below");
+			System.out.println("test: the current user is "+ curr_user);
+			log_in = true;
+			return log_in;
+		}
+		//System.out.println("")
+		//this.operations();
 		/*if (result.get(0).equals("host")){
 			this.hostOp();
 		}
@@ -96,48 +113,36 @@ public class User {
 	/**
 	 * Regist operation
 	 */
-	private void createUser(){
+	private boolean createUser(){
 		boolean user_exist;
 		String[] column_values = new String[user_column_name.length];
-		column_values = CommandLine.getInfo(user_column_name, user_column_name.length);
+		column_values = CommandLine.getInfo(user_column_name, 0, user_column_name.length-1).toArray(new String[0]);
 		// check if the user name is already existed, if so then choose another user name
 		do{
-			user_exist = CommandLine.sqlMngr.checkExist("*", new String[] {"user name"}, new String[] {column_values[Arrays.asList(user_column_name).indexOf("user name")]}, "users");
+			user_exist = sqlMngr.checkExist("*", new String[] {"user_name"}, new String[] {column_values[Arrays.asList(user_column_name).indexOf("user_name")]}, "users");
 			if(user_exist){
 				System.out.println("Sorry the user name already exist, please choose another user name");
-				String [] get_user = {"user name"};
-				column_values[Arrays.asList(user_column_name).indexOf("user name")] = CommandLine.getInfo(get_user, 1)[0];
+				String [] get_user = {"user_name"};
+				column_values[Arrays.asList(user_column_name).indexOf("user_name")] = CommandLine.getInfo(get_user,1,1).get(0);
 			}
 		}while(user_exist);
-		int row_affect = CommandLine.sqlMngr.insertOp("users", user_column_name, column_values);
+		int row_affect = sqlMngr.insertOp("users", user_column_name, column_values);
 		//set the user name that the user registed as the current user name once the user created a user
 		// the user will be auto logged in after register
-		this.setUser(column_values[Arrays.asList(user_column_name).indexOf("user name")]);
-		System.out.println("Regist sucessfully! Your user name is: " + this.getUser());
-		System.out.println("Please choose a operation that you want to do");
-		this.operations();
+		this.setUser(column_values[Arrays.asList(user_column_name).indexOf("user_name")]);
+		System.out.println("Regist sucessfully! Your user name is: " + this.curr_user);
+		return true;
 	}
 
-	public static String getUser(){
-		return user_id;
+	public  void setUser(String user){
+		curr_user = user;
 	}
 
-	public static void setUser(String user){
-		user_id = user;
-	}
-
-	/**
-	 * 
-	 */
-	private void operations(){
-		this.userMenu();
-
-	}
 
 	/**
 	 * it is the operations that the renter can do 
 	 */
-	private void userMenu() {
+	public void userMenu() {
 		System.out.println("=========OPERATIONS=========");
 		System.out.println("0. Log out.");
 		System.out.println("1. Make a booking.");
@@ -154,7 +159,7 @@ public class User {
 		System.out.print("Choose one of the previous options [0-11]: ");
 	}
 
-	private boolean hostNotice(){
+	public boolean hostNotice(){
 		boolean con = true;
 		String c_or_q;
 		System.out.println("Notice: this is an operation of the host type user. If you are not a host yet, do you want to become a host and continue?");
@@ -172,7 +177,7 @@ public class User {
 	}
 	//operation 0
 	// sql keeps connected after user logout
-	private void logOut(){
+	public void logOut(){
 		this.setUser(null);
 		for (int i = 0; i < 5; i++) {
 			System.out.println(".");
@@ -180,10 +185,10 @@ public class User {
 		System.out.println("Logged out successfully");
 		System.out.println("");
 		// Provide re-login/sign up options
-		String choice;
+		/*String choice;
 		boolean check = false;
-		choice = CommandLine.sc.nextLine();
 		System.out.println("Welcome to Mybnb, if you want to login press 1, if you are not a user yet please press 2 to register");
+		choice = CommandLine.sc.nextLine();
 		while (!check){
 			check = choice.equals("1") | choice.equals("2");
 		}
@@ -194,145 +199,143 @@ public class User {
 		else if (choice.equals("2")){
 			//create a user
 			this.createUser();
-		}
+		}*/
 	}
 
 
 	//operation 1
-	private void makeBooking(){
+	public void makeBooking(){
 
 	}
 	//operation 2/7
-	private void printHistory(int choice){
+	public void printHistory(int choice){
 		Booking b = new Booking();
 		// if the user chose 2 then print his/her rental history of the listings that he/she rented, else if the user chose 7 then print the rental history of the listings that he/she owns
 		if(choice == 2){
-			b.getRentalHistory();
+			b.getRentalHistory(this.curr_user);
 		}
 		else if (choice == 7){
-			b.getHostHistory();
+			b.getHostHistory(this.curr_user);
 		}
 	}
 
 	//operation 3/8
-	private void deleteFutureBooking(int choice){
+	public void deleteFutureBooking(int choice){
 		Booking b = new Booking();
 		if(choice == 3){
 			//delete a future booking as renter
-			b.deletBooking("renter");
+			b.deletBooking("renter", this.curr_user);
 		}
 		else if(choice == 8){
 			//delete a future booking as a host
-			b.deletBooking("host");
+			b.deletBooking("host", this.curr_user);
 		}
 	}
 
 	// if opt.4 is chosen, print his/her recent and completed rental histories first,
 	// else if it is opt.10, print all recent and completed rental histories of all listings that he/she owns
 	// then ask them to input ratings and comments
-	private void writeComments(int choice){
+	public void writeComments(int choice){
 		Comments c = new Comments();
 		Booking b = new Booking();
 		if (choice == 3) {
-			b.getRecentCompletedRentalHistory();
-			c.insertRateAndComments("renter");
+			b.getRecentCompletedRentalHistory(this.curr_user);
+			c.insertRateAndComments("renter", this.curr_user);
 
 		} else if (choice == 10) {
-			b.getRecentCompletedHostHistory();
-			c.insertRateAndComments("host");
+			b.getRecentCompletedHostHistory(this.curr_user);
+			c.insertRateAndComments("host", this.curr_user);
 		}
 	}
 
 	//operation 5
-	private void addListing(){
+	public void addListing(){
 		int l_ID;
 		float price;
 		Listing l = new Listing();
 		l_ID = l.addListing();
+		price = l.getAvePrice(l_ID);
 		ListCalendar c = new ListCalendar();
 		c.insertListCalendar(l_ID, price);
 
 	}
 
 	//operation 6
-	private void getHostListings(){
+	public void getHostListings(){
 		Listing l = new Listing();
-		l.getHostListings();
+		l.getHostListings(curr_user);
 
 	}
 
 	//operation 9
-	private void updateAvai(){
+	public void updateAvai(){
 		//check if the current user owns listings
 		boolean check;
 		boolean own_list;
 		boolean date_exist;
 		String date;
 		String status;
-		String y_q;
+		String d_q;
 		String delete_fu_b;
 		int listId = -1000;
-		check = sqlMngr.checkExist("*", new String[]{"host name"}, new String[] {User.user_id}, "listing");
+		check = sqlMngr.checkExist("*", new String[]{"host name"}, new String[] {curr_user}, "listing");
 		if(!check){
-			System.out.println("According our records, you are not a host yet. Please add listing first to become a host");
-			//TO Do:
-			//print the operation menue again and let the user re-choose an operation
-			this.operations();
-			return;
-		}
-		try{
-			do{
-				System.out.println("Please enter the listing ID");
-				listId = Integer.parseInt(CommandLine.sc.nextLine());
-				own_list = new Listing().ownList(listId, user_id);
-				if(!own_list){
-					System.out.println("According to our records. you do not own this listing, check if the listId is correct ");
-				}
-			}while(!own_list);
-		}catch(SQLException e){
-			System.out.println("Exception occurs in User.updateAvai");
-			e.printStackTrace();
-		}
-		do{
-			System.out.println("Please enter the date you want to change");
-			date = CommandLine.sc.nextLine();
-			date_exist = new ListCalendar().checkDate(listId, date);
-			if(!date_exist){
-				System.out.println("According to our records, the date is not open yet. The system only allow the host to open the future 2 months' calendar");
-			}
-		}while (!date_exist);
-		String original_status = new ListCalendar().checkStatus(listId, date);
-		System.out.println("The current status of the listing on that day is " + original_status);
-		do{
-			System.out.println("Please chose the status that you want to change. Type 'a' to change it availiable, or type 'u' to make it unavailiable");
-			status = CommandLine.sc.nextLine();
-		}while (!(status.equals("a") || status.equals("u")));
-		if(original_status.equals("b")){
-			System.out.println("The listing is booked on that day, do you want cancle this booking?");
-			do{
-				System.out.println("Type 'd' to delete the booking , type 'q' to quit and go back to operation menue padge");
-				y_q = CommandLine.sc.nextLine();
-			}while (!(y_q.equals("d") || y_q.equals("q")));
-			if (y_q.equals("y")){
-				//delete future booking
-				delete_fu_b = "DELETE FORM booking WHERE listing_id =  '"+listId+"' AND start_date <= '"+date+"' AND end_date >= '"+date+"';";
-				CommandLine.sqlMngr.excuteSql(delete_fu_b);
-				Booking b = new Booking();
-				b.updateCancellation("host");
-			}
-			else{
-				//quit and go back to operation menu
-				this.operations();
-				return;
-			}
+			System.out.println("According our records, you are not a host yet. Please add listing first to become a host. You will be sent to the operation menu page");
 		}
 		else{
-			new ListCalendar().updateAva(listId, date, status);
+			try{
+				do{
+					System.out.println("Following are the listings that you owned");
+					new Listing().getHostListings(curr_user);
+					System.out.println("Please enter the listing ID");
+					listId = Integer.parseInt(CommandLine.sc.nextLine());
+					own_list = new Listing().ownList(listId, curr_user);
+					if(!own_list){
+						System.out.println("According to our records. you do not own this listing, check if the listId is correct ");
+					}
+				}while(!own_list);
+			}catch(SQLException e){
+				System.out.println("Exception occurs in User.updateAvai");
+				e.printStackTrace();
+			}
+			do{
+				System.out.println("Please enter the date you want to change (yyyy-MM-dd)");
+				date = CommandLine.sc.nextLine();
+				date_exist = new ListCalendar().checkDate(listId, date);
+				if(!date_exist){
+					System.out.println("According to our records, the date is not open yet. The system only allow the host to open the future 2 months' calendar");
+				}
+			}while (!date_exist);
+			String original_status = new ListCalendar().checkStatus(listId, date);
+			System.out.println("The current status of the listing on that day is " + original_status);
+			do{
+				System.out.println("Please chose the status that you want to change. Type 'a' to change it availiable, or type 'u' to make it unavailiable");
+				status = CommandLine.sc.nextLine();
+			}while (!(status.equals("a") || status.equals("u")));
+			if(original_status.equals("b")){
+				System.out.println("The listing is booked on that day, do you want cancle this booking?");
+				do{
+					System.out.println("Type 'd' to delete the booking , type 'q' to quit and go back to operation menue padge");
+					d_q = CommandLine.sc.nextLine();
+				}while (!(d_q.equalsIgnoreCase("d") || d_q.equalsIgnoreCase("q")));
+				if (d_q.equalsIgnoreCase("d")){
+					//delete future booking
+					delete_fu_b = "DELETE FORM booking WHERE listing_id =  '"+listId+"' AND start_date <= '"+date+"' AND end_date >= '"+date+"';";
+					sqlMngr.excuteSql(delete_fu_b);
+					Booking b = new Booking();
+					b.updateCancellation("host", this.curr_user);
+				}
+				else{
+				}
+			}
+			else{
+				new ListCalendar().updateAva(listId, date, status);
+			}
 		}
 	}
 
 	//operation 11
-	private void updatePrice(){
+	public void updatePrice(){
 		String status;
 		System.out.println("Following are the listings that you own");
 		//TO DO:
