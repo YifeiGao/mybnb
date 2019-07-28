5662d7b924a59bf64a5e157454f35931e0d7b84f
YifeiGao <52712330+YifeiGao@users.noreply.github.com>
2019-07-28 16:19:09 -0400

    Update CommandLine.java

--- CommandLine.java
+++ CommandLine.java
@@ -10,17 +10,6 @@ import java.util.TimeZone;
 
 public class CommandLine {
 
-	private static final String[] renter_column_name = {"credit card number", "user name"};
-	private static final String[] renter_column_type = {"INT NOT NULL", "VARCHAR(30) NOT NULL"};
-	private static final String renter_primary_key = "credit card number";
-	private static final String [] rcomments_column_name = {"list coordinate", "list address"," renter user name", "comment date", "comment", "host user name", "list scale", "host scale"};
-	private static final String [] rcomments_column_type = {"VARCHAR(30) NOT NULL", "VARCHAR(30) NOT NULL", "VARCHAR(30) NOT NULL", "DATE NOT NULL", "TEXT NOT NULL", "VARCHAR(30) NOT NULL", "INT NOT NULL", "INT NOT NULL"};
-	private static final String rcomments_primary_key = "list coordinate, list address, renter user name, comment date";
-	private static final String[] hcomments_column_name = {"list coordinate", "list address"," renter user name", "comment date", "comment", "host user name", "renter scale"};
-	private static final String[] hcomments_column_type = {"VARCHAR(30) NOT NULL", "VARCHAR(30) NOT NULL", "VARCHAR(30) NOT NULL", "DATE NOT NULL", "TEXT NOT NULL", "VARCHAR(30) NOT NULL", "INT NOT NULL"};
-	private static final String hcomments_primary_key = null;
-
-
 	// 'sqlMngr' is the object which interacts directly with MySQL
 	public static SQLController sqlMngr = null;
 	// 'sc' is needed in order to scan the inputs provided by the user
@@ -54,85 +43,92 @@ public class CommandLine {
 	}
 
 	public boolean execute(){
+		sqlMngr.initialize();
 		if(sc != null && sqlMngr != null){
 			User u = new User();
 			int choice = -1;
 			String input = "";
-			boolean log_in;
-			log_in = u.userStart();
-			boolean con = true;
 			do{
-				input = sc.nextLine();
-				try{
-					choice = Integer.parseInt(input);
-					switch(choice){
-					case 0:
-						log_in = false;
-						u.logOut();
-						break;
-					case 1:
-						u.makeBooking();
-						break;
-					case 2:
-						u.printHistory(choice);
-						break;
-					case 3:
-						u.deleteFutureBooking(choice);
-						break;
-					case 4:
-						u.writeComments(choice);
-						break;
-					case 5:
-						con = u.hostNotice();
-						if(con){
-							u.addListing();
+				boolean log_in = false;
+				log_in = u.userStart();
+				boolean con = true;
+				if(log_in){
+					do{
+						u.userMenu();
+						input = sc.nextLine();
+						try{
+							choice = Integer.parseInt(input);
+							switch(choice){
+							case 0:
+								log_in = false;
+								u.logOut();
+								break;
+							case 1:
+								u.makeBooking();
+								break;
+							case 2:
+								u.printHistory(choice);
+								break;
+							case 3:
+								u.deleteFutureBooking(choice);
+								break;
+							case 4:
+								u.writeComments(choice);
+								break;
+							case 5:
+								con = u.hostNotice();
+								if(con){
+									u.addListing();
+								}
+								break;
+							case 6:
+								con = u.hostNotice();
+								if(con){
+									u.getHostListings();
+								}
+								break;
+							case 7:
+								con = u.hostNotice();
+								if(con){
+									u.printHistory(choice);
+								}
+								break;
+							case 8:
+								con = u.hostNotice();
+								if(con){
+									u.deleteFutureBooking(choice);
+								}
+								break;
+							case 9:
+								con = u.hostNotice();
+								if(con){
+									u.updateAvai();
+								}
+								break;
+							case 10:
+								con = u.hostNotice();
+								if(con){
+									u.writeComments(choice);
+								}
+								break;
+							case 11:
+								con = u.hostNotice();
+								if(con){
+									u.updatePrice();
+								}
+								break;
+							default:
+								break;
+							}
+						}catch (NumberFormatException e){
+							input = "-1";
+							System.err.println("NumberFormatException occurs in CommandLine.excute");
+							e.printStackTrace();
 						}
-						break;
-					case 6:
-						con = u.hostNotice();
-						if(con){
-							u.getHostListings();
-						}
-						break;
-					case 7:
-						con = u.hostNotice();
-						if(con){
-							u.printHistory(choice);
-						}
-						break;
-					case 8:
-						con = u.hostNotice();
-						if(con){
-							u.deleteFutureBooking(choice);
-						}
-						break;
-					case 9:
-						con = u.hostNotice();
-						if(con){
-							u.updateAvai();
-						}
-						break;
-					case 10:
-						con = u.hostNotice();
-						if(con){
-							u.writeComments(choice);
-						}
-						break;
-					case 11:
-						con = u.hostNotice();
-						if(con){
-							u.updatePrice();
-						}
-						break;
-					default:
-						break;
-					}
-				}catch (NumberFormatException e){
-					input = "-1";
-					System.err.println("NumberFormatException occurs in CommandLine.excute");
-					e.printStackTrace();
+					}while(log_in);
 				}
-			}while(log_in);
+				System.out.println("Please type anything to go to the user start page");
+			}while(!sc.next().equalsIgnoreCase("EXIT"));
 			return true;
 		}
 		else {
